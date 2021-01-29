7681067fd196b2c534a5e30b376fe289cc9cad29
Eric Carlson <carlsoneric@protonmail.com>
2021-01-28 20:56:34 -0700

    Save response as pandas object

--- oshwa_api.py
+++ oshwa_api.py
@@ -2,13 +2,14 @@ import json
 import csv
 import requests
 from datetime import date
+import ast
 
 url = "https://certificationapi.oshwa.org/api/projects"
 
 payload = {}
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer <Token>'
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYmFkMjg5OGU3ZWM4MDAxN2IwZGY0ZiIsImlhdCI6MTYwNjA3OTExNCwiZXhwIjoxNjE0NzE5MTE0fQ.tQWkMcx8eVH0YBcDUJJY2wkMU7VRcKsFxG53L1iKONM'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
@@ -21,6 +22,6 @@ for element in data:
 
 with open('oshwa_api_' + str(date.today()) + '.csv', 'w',encoding="utf-8") as f:
     writer = csv.writer(f,quoting=csv.QUOTE_ALL)
-    writer.writerow(["UID","Creator","Country","Project Name","Website","Version","Description","Primary Types","Additional Types","Documentation","Hardware License","Software License","Documentation License","Certification Date"])
+    writer.writerow(["UID","Creator","Country","Project Name","Website","Version","Description","Primary Type","Additional Types","Documentation","Hardware License","Software License","Documentation License","Certification Date"])
     for row in data:
         writer.writerow(row.values())
