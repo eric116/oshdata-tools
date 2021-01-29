30830f3eaf7def04e8a4d651838655c6e3380dbc
Eric Carlson <carlsoneric@protonmail.com>
2021-01-28 21:15:43 -0700

    Use panadas object

--- oshwa_api.py
+++ oshwa_api.py
@@ -1,27 +1,18 @@
 import json
-import csv
 import requests
 from datetime import date
-import ast
+import pandas as pd
 
 url = "https://certificationapi.oshwa.org/api/projects"
 
 payload = {}
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYmFkMjg5OGU3ZWM4MDAxN2IwZGY0ZiIsImlhdCI6MTYwNjA3OTExNCwiZXhwIjoxNjE0NzE5MTE0fQ.tQWkMcx8eVH0YBcDUJJY2wkMU7VRcKsFxG53L1iKONM'
+    'Authorization': 'Bearer <token>'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
-data = json.loads(response.text.encode('utf8'))
-for element in data:
-    element.pop('publicContact', None)
-    element.pop('previousVersions', None)
-    element.pop('citations', None)
-
-with open('oshwa_api_' + str(date.today()) + '.csv', 'w',encoding="utf-8") as f:
-    writer = csv.writer(f,quoting=csv.QUOTE_ALL)
-    writer.writerow(["UID","Creator","Country","Project Name","Website","Version","Description","Primary Type","Additional Types","Documentation","Hardware License","Software License","Documentation License","Certification Date"])
-    for row in data:
-        writer.writerow(row.values())
+pdObj = pd.read_json(response.content, orient='records')
+csvData = pdObj.to_csv('oshwa_api_' + str(date.today()) + '.csv', index=False)
+print("Write successful")
